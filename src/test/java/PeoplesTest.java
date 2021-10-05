ee59845e27ac4252b989a398fe65ea5fe9d9f90c
jkarlokc <jkarlokc@gmail.com>
2021-10-05 13:55:28 +0100

    Added ResourcesClass

--- src/test/java/PeoplesTest.java
+++ src/test/java/PeoplesTest.java
@@ -1,8 +1,14 @@
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class PeoplesTest {
+    @BeforeClass
+    public void Setup(){
+        Resources resources = new Resources();
+    }
     @Test
     public void test1(){
         System.out.println("HelloWorld");
+
     }
 }
---
+++ src/test/java/Resources.java
@@ -0,0 +1,7 @@
+public class Resources {
+    private String baseUrl = "https://ghibliapi.herokuapp.com";
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+}
