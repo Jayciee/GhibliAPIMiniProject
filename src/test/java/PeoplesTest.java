2b318bb9a2d2deca31e2b0c3c9ccd620950f7e6b
jkarlokc <jkarlokc@gmail.com>
2021-10-05 14:02:56 +0100

    FirstPeoplesTest

--- src/test/java/PeoplesTest.java
+++ src/test/java/PeoplesTest.java
@@ -1,14 +1,20 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class PeoplesTest {
+    private String url;
     @BeforeClass
     public void Setup(){
         Resources resources = new Resources();
+        url = resources.getBaseUrl();
     }
     @Test
-    public void test1(){
-        System.out.println("HelloWorld");
-
+    public void GivenCorrectBaseUrl(){
+        Response response = RestAssured.get(url);
+        int statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode,200);
     }
 }
